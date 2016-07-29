ab9b7d17f7da3683dd196f1c6948bc366bc33443
Julia Wilkins <juliawilkins2018@u.northwestern.edu>
2016-07-29 14:08:49 -0400

    bugs

--- setup.py
+++ setup.py
@@ -24,6 +24,6 @@ setup(
     app=APP,
     data_files=DATA_FILES,
     options={'py2app': OPTIONS},
-    setup_requires=['py2app'],
+    #setup_requires=['py2app'],
     #install_requires =['pyyaml', 'sox', 'numpy']
 )
